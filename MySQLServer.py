b33eabb2374a94a0d7492fe36d80b228a74231d1
Tech-Savvy <wilkinsondari7@gmail.com>
2025-01-27 21:34:20 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,9 +10,16 @@ try:
         password="Rhs@11341",
         database="alx_book_store"
     )
-
     print("Database 'alx_book_store' created successfully!")
     mydb.close()
 except Error as e:
-    print(f'Server connection failed, Error: {e}')
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Rhs@11341")
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")    
+    
+
+
 
