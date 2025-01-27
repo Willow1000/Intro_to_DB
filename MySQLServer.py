af8b7aa09ffdad6cccc8fe8d4af4f68959d6e97d
Tech-Savvy <wilkinsondari7@gmail.com>
2025-01-27 21:39:08 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,22 +3,28 @@ from mysql.connector import Error
 
 
 try:
-
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Rhs@11341",
-        database="alx_book_store"
-    )
-    print("Database 'alx_book_store' created successfully!")
-    mydb.close()
-except Error as e:
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Rhs@11341")
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")    
+    try:
+        
+        mydb=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Rhs@11341",
+            database="alx_book_store"
+        )
+        print("Database 'alx_book_store' created successfully!")
+        mydb.close()
+    except Error as e:
+        mydb=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Rhs@11341")
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")  
+        print("Database 'alx_book_store' created successfully!")
+        mydb.close()
+        mycursor.close()
+except mysql.connector.Error as e:
+    print(f'An error occurred {e}')
     
 
 
